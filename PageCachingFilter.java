fa84b63221845c977c9d1c305354ddfc78cf7134
wutext <36761963+wutext@users.noreply.github.com>
2018-02-23 06:28:06 -0600

    taggle a java file
    contest : taggle a java file

---
+++ PageCachingFilter.java
@@ -0,0 +1,20 @@
+package com.sunwiseinfo.wing_sv.common.filter;
+
+import com.sunwiseinfo.wing_sv.common.utils.CacheUtils;
+
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.constructs.web.filter.SimplePageCachingFilter;
+
+/**
+ * 页面高速缓存过滤器
+ * @author wing_sv
+ * @version 2013-8-5
+ */
+public class PageCachingFilter extends SimplePageCachingFilter {
+
+	@Override
+	protected CacheManager getCacheManager() {
+		return CacheUtils.getCacheManager();
+	}
+	
+}
